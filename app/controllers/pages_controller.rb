c5b121844d367a1ac4fd33ea6e9c281c6b05444f
Sean McGregor <smcgregor08@cmc.edu>
2011-12-28 19:52:20 -0800

    added more content and roadmap

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,48 +3,62 @@ class PagesController < ApplicationController
   before_filter :authenticate_user!, :only => [:account]
   
   def faq
+    @sidebar = {:news => false, :help => true}
   end
 
   def join
+    @sidebar = {:news => false, :contribute => true}
   end
 
   def roadmap
+    @sidebar = {:news => false, :about => true}
   end
 
   def people
+    @sidebar = {:news => false, :about => true}
   end
 
   def license
+    @sidebar = {:news => false, :about => true}
   end
 
   def privacy
+    @sidebar = {:news => false, :about => true}
   end
 
   def terms
+    @sidebar = {:news => false, :about => true}
   end
 
   def help
+    @sidebar = {:news => false, :help => true}
   end
 
   def status
+    @sidebar = {:news => false, :help => true}
   end
 
   def irc
+    @sidebar = {:news => false, :contribute => true}
   end
 
   def bug
+    @sidebar = {:news => false, :contribute => true}
   end
 
   def donate
+    @sidebar = {:news => false, :contribute => true}
   end
 
   def download
   end
 
   def about
+    @sidebar = {:news => false, :about => true}
   end
 
   def email
+    @sidebar = {:news => false, :help => true}
   end
   
   def account
