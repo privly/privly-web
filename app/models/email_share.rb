f577af4cc436d580799db13d21fdb5baa637e8b3
Sean McGregor <smcgregor08@cmc.edu>
2012-02-07 09:48:42 -0800

    protected attributes on email shares

--- app/models/email_share.rb
+++ app/models/email_share.rb
@@ -6,4 +6,8 @@ class EmailShare < ActiveRecord::Base
   validates :email, :uniqueness => { :scope => :post_id,
       :message => "must be unique." }
   validates :post_id, :email, :presence => true
+  
+  #effectively protects timestamps, access to email share is controlled by CanCan
+  attr_accessible :post_id, :email, :can_show, :can_destroy, :can_update, :can_share
+
 end
