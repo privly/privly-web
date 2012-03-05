e54cc07747eb1a1c058d3bdebb93f3f1ca1b93e4
Sean McGregor <smcgregor08@cmc.edu>
2012-03-04 23:18:53 -0800

    changed iframe captcha behavior to open a new window to better support chrome

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -172,6 +172,7 @@ class PostsController < ApplicationController
       elsif verify_recaptcha
         session[:person] = true
         session[:robot_count] = 1
+        flash[:notice] = "You are human!"
       end
       
       if @post 
