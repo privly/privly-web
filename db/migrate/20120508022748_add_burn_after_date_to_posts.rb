c50cf1c786000507513401fbdc3ab0ec9d6cd4b2
Sean McGregor <smcgregor08@cmc.edu>
2012-06-22 12:02:35 -0700

    removed timed deletion from migration and changed the staging branch to master

--- db/migrate/20120508022748_add_burn_after_date_to_posts.rb
+++ db/migrate/20120508022748_add_burn_after_date_to_posts.rb
@@ -8,14 +8,14 @@ class AddBurnAfterDateToPosts < ActiveRecord::Migration
     
     add_column :posts, :burn_after_date, :datetime
     
-    users = User.where(:admin => false)
-    for user in users
-      posts = user.posts
-      for post in posts
-        post.burn_after_date = Time.now + 14.days
-        post.save
-      end
-    end
+    #users = User.where(:admin => false)
+    #for user in users
+    #  posts = user.posts
+    #  for post in posts
+    #    post.burn_after_date = Time.now + 14.days
+    #    post.save
+    #  end
+    #end
     
   end
 end
