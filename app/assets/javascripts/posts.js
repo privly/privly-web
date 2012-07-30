346ed431b90a7024365659ee379cc475cd600017
Sean McGregor <smcgregor08@cmc.edu>
2012-07-29 23:31:43 -0700

    added prompt for downloading extension to the bottom of the layout and moved navigation to the sidebar

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,3 +1,122 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+//This file was recently moved from the post's show action and needs
+//refactoring to become library functions
+
+/**
+ * Converts an associative array to an encoded string
+ * for appending to the anchor.
+ *
+ * @param object associative_array Object to be serialized
+ * @return string
+ */
+function hashToParameterString(associativeArray)
+{
+  var parameterString = ""
+  for (key in associativeArray)
+  {
+      if( parameterString === "" )
+      {
+        parameterString = encodeURIComponent(key);
+        parameterString += "=" + encodeURIComponent(associativeArray[key]);
+      } else {
+        parameterString += "&" + encodeURIComponent(key);
+        parameterString += "=" + encodeURIComponent(associativeArray[key]);
+      }
+  }
+  
+  //padding for URL shorteners
+  if (parameterString !== "") {
+    parameterString += "&p=p";
+  }
+  
+  return parameterString;
+}
+
+/**
+ * Converts a string to an associative array.
+ *
+ * @param string parameter_string String containing parameters
+ * @return object
+ */
+function parameterStringToHash(parameterString)
+{
+  var parameterHash = {};
+  var parameterArray = parameterString.split("&");
+  for (var i = 0; i < parameterArray.length; i++) {
+    //var currentParamterString = decodeURIComponent(parameterArray[i]);
+    var pair = parameterArray[i].split("=");
+    var key = decodeURIComponent(pair[0]);
+    var value = decodeURIComponent(pair[1]);
+    parameterHash[key] = value;
+  }
+
+  return parameterHash;
+}
+
+/**
+ * Get an associative arroy of the parameters found in the anchor
+ *
+ * @return object
+ **/
+function getParameterHash()
+{
+  var hashIndex = window.location.href.indexOf("#");
+  if (hashIndex >= 0) {
+    return parameterStringToHash(window.location.href.substring(hashIndex + 1));
+  } else {
+    return {};
+  }
+}
+
+/**
+ * Fills the link options form with the current parameter values and
+ * deactivates the pre-filled parameters.
+ */
+function fillFormStartingValues()
+{
+  var currentParameters = getParameterHash();
+  var elem = document.getElementById('linkFormatterForm').elements;
+  for(var i = 0; i < elem.length; i++)
+  {
+      if(elem[i].getAttribute("use_in_url") == "true")
+      {
+        if (currentParameters[elem[i].name] !== undefined) {
+          elem[i].value = currentParameters[elem[i].name];
+          elem[i].disabled = true;
+        }
+      }
+  }
+}
+
+/**
+ * Collects the parameters from the "Link Options" sidebar and
+ * assigns the newFormattedLink element to the generated URL
+ */
+function updateURL()
+{
+    var parameterObject = {};
+    var elem = document.getElementById('linkFormatterForm').elements;
+    for(var i = 0; i < elem.length; i++)
+    {
+        if(elem[i].getAttribute("use_in_url") == "true")
+        {
+            var val = elem[i].value;
+            if(! val)
+                continue;
+            if(elem[i].type == "checkbox" && elem[i].checked != true)
+                continue;
+            parameterObject[elem[i].name] = val;
+        }
+    }
+    
+    var anchorIndex = window.location.href.indexOf("#");
+    if (anchorIndex < 0) {
+      anchorIndex = window.location.href.length + 1;
+    }
+    
+    var newUrl = 
+      window.location.href.substring(0, anchorIndex) + 
+      "#" +
+      hashToParameterString(parameterObject);
+    document.getElementById('newFormattedLink').innerHTML = newUrl;
+}
+
