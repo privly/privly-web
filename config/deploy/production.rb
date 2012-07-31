ebf25e94acb1cd95f19fe12a9d4c407e72dfb2ee
Sean McGregor <smcgregor08@cmc.edu>
2012-07-31 13:57:21 -0700

    fixes for deployment to production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,4 +4,4 @@ role :web, "priv.ly"                          # Your HTTP server, Apache/etc
 role :app, "priv.ly"                          # This may be the same as your `Web` server
 role :db,  "priv.ly", :primary => true # This is where Rails migrations will run
 
-set :branch, "kickstarter"
+set :branch, "master"
