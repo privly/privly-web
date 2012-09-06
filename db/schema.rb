64773b4e44c27dace653ad7f489cbff9a2d11740
Sean McGregor <smcgregor08@cmc.edu>
2012-09-05 23:09:36 -0700

    updated Gemfile and rolled back sass-rails to older version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120827224815) do
+ActiveRecord::Schema.define(:version => 20120906045507) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
