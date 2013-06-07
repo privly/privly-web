fa608bfd9221718460654266be82b24841a8dbdb
Sean McGregor <smcgregor08@cmc.edu>
2013-06-07 16:16:13 -0700

    fixed some coding style

--- app/assets/javascripts/share.js
+++ app/assets/javascripts/share.js
@@ -12,16 +12,18 @@ var validIpAdressRegex = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){
   */
 function identityIdAndMessage(identity, messageNode, hiddenElement)
 {
-  //check to enable or disable 'Create Share' button, this button will be enabled if there is any text in textfield
+  //check to enable or disable 'Create Share' button, 
+  //this button will be enabled if there is any text in textfield
   if(identity.split(' ').join('').length > 0){
      $('#create_share_button').removeAttr('disabled');
-  }
-  else{
+  } else {
       $('#create_share_button').attr('disabled','disabled');
   }
 
   if ( identity.indexOf(" ") > 0 || identity.indexOf(",") > 0) {
-    //write new block to toggle which form element is displayed, carry over values, register/unregister event listeners
+    
+    //write new block to toggle which form element is displayed, 
+    //carry over values, register/unregister event listeners
     messageNode.text("List of Shares (comma or space separated)");
     hiddenElement.val("");
   } 
