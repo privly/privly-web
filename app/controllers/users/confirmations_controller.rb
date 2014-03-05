1accd07f067304fe25e349549966c6b53f04503b
Sean McGregor <smcgregor08@cmc.edu>
2014-03-04 18:00:37 -0800

    added documentation to confirmations controller

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,10 +1,16 @@
+# Confirmations tie a user account to a confirmed email address. 
+# Usually the Devise gem handles sending the confirmations, but since 
+# we don't want to send the confirmation link if the user's account will
+# not be active, we have to block the "create" endpoint that is defined
+# by devise.
 class Users::ConfirmationsController < Devise::ConfirmationsController
 
   # == Create a confirmation.
   #
   # When a user tries to resend confirmation instructions, it has 
   # to be checked if that user's account has been approved by the
-  # administrator, otherwise it has to be redirected back.
+  # administrator or issued by an invitation, otherwise it has to
+  # be redirected back.
   #
   # === Routing  
   #
@@ -21,24 +27,19 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   # * Default: nil
   # The email of the user that wants confirmation instructions to be resent
   def create
-
+    
     email = params[:user][:email]
     user = User.find_by_email(email)
-
-    # Check if the user has previously signed up
-    if not user
-    	redirect_to new_user_session_path, :notice => "If your e-mail exists on our database, you will receive an email with instructions about how to confirm your account in a few minutes."
-
+    
     # If the account is approved account, call the 'create' method from
     # Devise's ConfirmationsController 
-    elsif user.can_post
+    if user and user.can_post
       super
-
-    # else print appropriate message
     else
-      redirect_to new_user_session_path, :notice => "If your e-mail exists on our database, you will receive an email with instructions about how to confirm your account in a few minutes."
+      redirect_to new_user_session_path, 
+        :notice => "If your e-mail exists on our database, you will receive an email with instructions about how to confirm your account in a few minutes."
     end
     
   end
-
+  
 end
