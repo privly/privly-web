3def067f9f175799f563f6b3ca7d85d8f389fbc1
Sean McGregor <smcgregor08@cmc.edu>
2014-10-13 09:43:49 -0700

    removed Karma test runner

--- test/karma/install.js
+++
@@ -1,6 +0,0 @@
-// Creates sauce.json if not yet present after install
-var fs = require('fs');
-
-if (!fs.existsSync('sauce.json')) {
-    fs.writeFileSync('sauce.json', fs.readFileSync('sauce-sample.json'));
-}
