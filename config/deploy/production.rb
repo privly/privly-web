10c5012ff55adb3239b53bf900a34520fdbcf4f8
Sean McGregor <gitcommit@seanbmcgregor.com>
2015-12-27 20:12:20 -0800

    updated Capfile and deploy scripts

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,65 @@
-server 'privlyalpha.org', :app, :web, :primary => true
-set :domain, "privlyalpha.org"
-role :web, "privlyalpha.org"                          # Your HTTP server, Apache/etc
-role :app, "privlyalpha.org"                          # This may be the same as your `Web` server
-role :db,  "privlyalpha.org", :primary => true # This is where Rails migrations will run
+# server-based syntax
+# ======================
+# Defines a single server with a list of roles and multiple properties.
+# You can define all roles on a single server, or split them:
 
-set :branch, "master"
+server 'privlyalpha.org', user: ENV["PRIVLY_DEPLOY_USER"], roles: %w{app db web}, port: ENV["PRIVLY_DEPLOY_PORT"]
+# server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
+# server 'db.example.com', user: 'deploy', roles: %w{db}
+set :stage, :production
+set :db, :production
+
+set :branch, ENV["REVISION"] || ENV["BRANCH_NAME"] || "master"
+
+set :privly_shared_path, "/var/www/shared"
+
+# role-based syntax
+# ==================
+
+# Defines a role with one or multiple servers. The primary server in each
+# group is considered to be the first unless any  hosts have the primary
+# property set. Specify the username and a domain or IP for the server.
+# Don't use `:all`, it's a meta role.
+
+# role :app, %w{deploy@example.com}, my_property: :my_value
+# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
+# role :db,  %w{deploy@example.com}
+
+
+
+# Configuration
+# =============
+# You can set any configuration variable like in config/deploy.rb
+# These variables are then only loaded and set in this stage.
+# For available Capistrano configuration variables see the documentation page.
+# http://capistranorb.com/documentation/getting-started/configuration/
+# Feel free to add new variables to customise your setup.
+
+
+
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult the Net::SSH documentation.
+# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
+#
+# Global options
+# --------------
+#  set :ssh_options, {
+#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+#    forward_agent: false,
+#    auth_methods: %w(password)
+#  }
+#
+# The server-based syntax can be used to override options:
+# ------------------------------------
+# server 'example.com',
+#   user: 'user_name',
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: 'user_name', # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: 'please use keys'
+#   }
